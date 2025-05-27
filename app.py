e77ec9134f00965d700e7a1eb6421b76e2056e30
Zxc Iop <zxciop@users.noreply.huggingface.co>
2025-05-27 18:40:09 +0000

    Update app.py

--- app.py
+++ app.py
@@ -1,10 +1,14 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, send_from_directory
 
-app = Flask(__name__, static_url_path='/static')
+app = Flask(__name__, static_url_path='/static', static_folder='static')
 
 @app.route('/')
-def home():
-    return render_template('/templates/index.html')
+def index():
+    return render_template('index.html')
+
+@app.route('/models/<path:filename>')
+def serve_model(filename):
+    return send_from_directory('static/models', filename)
 
 if __name__ == '__main__':
-    app.run(debug=True)
+    app.run(host='0.0.0.0', port=7860)  # 👈 Important: use port 7860
