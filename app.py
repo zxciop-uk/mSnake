7bdae677dd13e24a760bde31effaeb06555405eb
Zxc Iop <zxciop@users.noreply.huggingface.co>
2025-05-27 18:14:30 +0000

    Update app.py

--- app.py
+++ app.py
@@ -4,7 +4,7 @@ app = Flask(__name__, static_url_path='/static')
 
 @app.route('/')
 def home():
-    return render_template('index.html')
+    return render_template('/templates/index.html')
 
 if __name__ == '__main__':
     app.run(debug=True)
